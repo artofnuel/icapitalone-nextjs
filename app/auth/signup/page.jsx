df392fdfc1ec0bd9f3b2055a4c211a8695ff2a8e
Emmanuel Inua <artofnuel@gmail.com>
2024-05-04 21:21:22 +0100

    Update page.jsx

--- app/auth/signup/page.jsx
+++ app/auth/signup/page.jsx
@@ -44,7 +44,7 @@ const Page = () => {
     try {
       const response = await fetch(
         // "http://localhost:3020/auth/signup" ||
-        "https://icapitalone-nextjs.vercel.app/auth/signup",
+        "http://127.0.0.1:3020/auth/signup",
         {
           method: "POST",
           headers: {
